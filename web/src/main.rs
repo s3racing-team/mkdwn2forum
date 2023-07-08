ef4ffe1403dc5983dd6f16e3f0182a807788013e
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2023-07-08 22:26:51 +0200

    feat: add forum placeholder

--- web/src/main.rs
+++ web/src/main.rs
@@ -30,10 +30,32 @@ fn app() -> Html {
         })
     };
 
+    let markdown_placeholder = "\
+# This is a heading
+
+## This is a subheading
+
+[Here goes the link text](https://here-goes-the-url.com)
+<https://this-is-a-plain-url.com>
+
+- here
+- is
+- a
+- list
+
+1. And
+2. this
+3. is
+4. a
+5. numbered
+6. list
+";
+    let forum_placeholder = mkdwn2forum::convert(markdown_placeholder);
+
     html! {
         <div class="container">
-            <textarea class="left" oninput={on_text_input} placeholder="# This is a heading\n## This is a subheading\n[Here goes the link text](https://here-goes-the-url.com)\n<https://this-is-a-plain-url.com>\n\n- here\n- is\n- a\n- list\n\n1. And\n2. this\n3. is\n4. a\n5. numbered\n6. list\n" />
-            <textarea class="right" value={output.to_string()} readonly=true />
+            <textarea class="left" oninput={on_text_input} placeholder={markdown_placeholder} />
+            <textarea class="right" value={output.to_string()} readonly=true placeholder={forum_placeholder} />
             <button class="copy_button" onclick={copy_text}>
                 <svg class="copy_icon" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512">
                     <path d="M272 0H396.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H272c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128H192v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z"/>
