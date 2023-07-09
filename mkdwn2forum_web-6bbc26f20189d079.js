72922b386535fd1e4b52fb15dc71cf553504d852
Saecki <Saecki@users.noreply.github.com>
2023-07-09 14:50:39 +0000

    deploy: 949d951432070216be81a463a4d37e53858b7637

--- mkdwn2forum_web-6bbc26f20189d079.js
+++ mkdwn2forum_web-6bbc26f20189d079.js
@@ -647,7 +647,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('mkdwn2forum_web-5062ec4d67ac49f6_bg.wasm', import.meta.url);
+        input = new URL('mkdwn2forum_web-6bbc26f20189d079_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
